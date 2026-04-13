5522e8c23fd58cb87f028b8bea965ae32f610245
rodrigotn-ribas <rodrigotn.ribas@gmail.com>
2026-04-13 10:42:51 -0300

    final project

--- 16_day/oop-coffee-machine-start/main.py
+++ 16_day/oop-coffee-machine-start/main.py
@@ -2,13 +2,23 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-order = Menu()
-coffee_maker = CoffeeMaker()
+# Objects
+order = Menu() # create an object of the class Menu()
+coffee_maker = CoffeeMaker() # create an object of the class CoffeeMaker()
+money_machine = MoneyMachine() # create an object of the class MoneyMachine()
 
-print(order)
+is_on = True
 
-order_choice = input(f"What would you like? ({order.get_items()}): ")
-print(order.find_drink(order_choice))
-
-
-coffee_maker.make_coffee(order)
+while is_on:
+    choice = input(f"What would you like? ({order.get_items()}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report() # report the ingredients
+        money_machine.report() # report the money
+    else:
+        drink = order.find_drink(choice)  # return an object of the class MenuItem()
+        if coffee_maker.is_resource_sufficient(drink):
+            is_transaction_successful = money_machine.make_payment(drink.cost) # try to make the payment
+            if is_transaction_successful:
+                coffee_maker.make_coffee(drink) # makes the coffee
