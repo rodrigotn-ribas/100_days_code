b4bd4620762f5be564e1890f1ecfaa098e629b71
rodrigotn-ribas <rodrigotn.ribas@gmail.com>
2026-04-22 09:57:13 -0300

    udemy course 122. Quiz Project part 3

---
+++ 17_day/quiz-game-start/main.py
@@ -0,0 +1,13 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+question_number = 0
+for question in question_data:
+    question_text = question['text']
+    question_answer = question['answer']
+    new_question = Question(q_text = question_text, q_answer = question_answer)
+    question_bank.append(new_question)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
