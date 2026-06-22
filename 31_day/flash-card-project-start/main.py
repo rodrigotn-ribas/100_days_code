698855eaa6ebf10b02bfa759cd2d8f459a27dc67
rodrigo.ribas <rodrigo.ribas@ecoa.puc-rio.br>
2026-06-22 17:30:58 -0300

    241. Step 4 - Save Your Progress

--- 31_day/flash-card-project-start/main.py
+++ 31_day/flash-card-project-start/main.py
@@ -9,7 +9,7 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pd.read_csv("data/hsk1.csv")
 to_learn = data.to_dict(orient="records")
-current_card = random.choice(to_learn)
+current_card = {}
 
 
 # ------------------------- Button Function --------------------------- #
@@ -19,11 +19,12 @@ def next_card():
     canvas.itemconfig(canvas_image, image=card_front_img)
     canvas.itemconfig(card_title, text="Mandarim", fill="black")
     canvas.itemconfig(card_word, text=current_card["Mandarim"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
     canvas.itemconfig(pinyin_word_label, text=current_card["Pinyin"], fill="black")
-    window.after(5000, flip_card)
+    window.after(3000, func=flip_card)
+
 
 def flip_card():
-    global current_card
     canvas.itemconfig(canvas_image, image=card_back_img)
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
@@ -33,11 +34,13 @@ window = Tk()
 window.title("Flashly")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 # Canvas
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR) # Image size
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
-canvas_image = canvas.create_image(400, 263, image=card_front_img)
+card_background = canvas_image = canvas.create_image(400, 263, image=card_front_img)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 card_title = canvas.create_text(400, 120, text="Title", font=("Arial", 40, "italic"))
@@ -54,6 +57,5 @@ known_button = Button(image=check_image, highlightthickness=0, command=next_card
 known_button.grid(column=1, row=1)
 
 next_card()
-window.after(3000, flip_card)
 
 window.mainloop()
