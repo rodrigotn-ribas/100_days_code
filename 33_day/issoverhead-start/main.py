628e2ace1c1ef3903ea20698f65f271f7cbd7166
rodrigotn-ribas <rodrigotn.ribas@gmail.com>
2026-06-30 16:50:13 -0300

    259. Day 34 Goals: what you will make by the end of the day

--- 33_day/issoverhead-start/main.py
+++ 33_day/issoverhead-start/main.py
@@ -1,43 +1,70 @@
+import time
 import requests
-from datetime import datetime
+from datetime import datetime, timezone
+import smtplib
 
 MY_LAT = 51.507351 # Your latitude
 MY_LONG = -0.127758 # Your longitude
+MY_EMAIL = 'smartcitybots@gmail.com'
+MY_PASSWORD = 'ubgc haug eafb dwly '
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
+def is_close():
+    try:
+        response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    except requests.RequestException as e:
+        print(e)
+    else:
+        response.raise_for_status()
+        data = response.json()
 
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
-# print(iss_longitude, iss_latitude)
-# print(MY_LAT, MY_LONG)
+        iss_latitude = float(data["iss_position"]["latitude"])
+        iss_longitude = float(data["iss_position"]["longitude"])
+        print(iss_latitude, iss_longitude)
+        # print(MY_LAT,MY_LONG)
 
-lat_dif = (abs(iss_latitude) - abs(MY_LAT))
-lng_dif = (abs(iss_longitude) - abs(MY_LONG))
-dif_pos = (lat_dif, lng_dif)
-#Your position is within +5 or -5 degrees of the ISS position.
-if dif_pos[0] >= 5 or dif_pos[0] > 5 or dif_pos[1] < 5 or dif_pos[1] > 5:
+        lat_dif = abs(iss_latitude - MY_LAT)
+        lng_dif = abs(iss_longitude - MY_LONG)
 
+        #Your position is within +5 or -5 degrees of the ISS position.
+        return MY_LAT <= iss_latitude <= MY_LAT+5 and MY_LONG <= iss_longitude <= MY_LONG+5
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+def is_dark():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    try:
+        response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    except requests.RequestException as e:
+        print(e)
+    else:
+        response.raise_for_status()
+        data = response.json()
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+        sunrise = datetime.fromisoformat(data["results"]["sunrise"])
+        sunset = datetime.fromisoformat(data["results"]["sunset"])
 
-time_now = datetime.now()
+        now = datetime.now(timezone.utc)
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
+        return now >= sunset or now <= sunrise
+
+def send_email():
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs="rodrigotn.ribas@gmail.com",
+            msg=f"Subject:International space station is here\n\n"
+                f"Look up, iss is on your sky"
+        )
+
+while True:
+    if is_close() and is_dark():
+        send_email()
+
+    time.sleep(60)
 
 
 
